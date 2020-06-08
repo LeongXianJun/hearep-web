d296d214d97c63e2a2f6e69fe44c483e0f3854b6
LeongXianJun <leongxianjun@1utar.my>
2020-06-08 18:06:16 +0800

    Store

--- src/stores/index.tsx
+++ src/stores/index.tsx
@@ -1,2 +1,5 @@
 export { default as UserStore } from './UserStore'
 export * from './UserStore'
+
+export { default as HealthRecordStore } from './HealthRecordStore'
+export * from './HealthRecordStore'
