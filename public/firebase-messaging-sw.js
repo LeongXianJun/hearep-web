278409f31378fcfd580b87db1dc83d1018438657
LeongXianJun <leongxianjun@1utar.my>
2020-06-28 20:01:58 +0800

    Update Background Handler

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/7.14.5/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/7.14.5/firebase-messaging.js')
 
-console.log(process?.env ?? 'GG')
-
 firebase.initializeApp({
   messagingSenderId: '803146768068'
 })
@@ -10,11 +8,9 @@ firebase.initializeApp({
 firebase.messaging().setBackgroundMessageHandler(payload =>
 {
   const { data } = payload
-  // console.log('Message received. 2', data)
   const notificationTitle = data.title
   const notificationOptions = {
-    body: data.description,
-    icon: '/resources/logo/icon.svg'
+    body: data.description
   }
 
   if (Notification.permission !== 'denied')
